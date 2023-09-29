e4d40b5d271e3ff8cb7263f4f9c62c0aa5630dfe
Roman Bernard <144805377+Roman2711@users.noreply.github.com>
2023-09-29 15:48:09 +0200

    buttons

--- ROS/Python/test_TeleopNode.py
+++ ROS/Python/test_TeleopNode.py
@@ -18,15 +18,24 @@ class TeleopNode:
         axes = [round(value,3) for value in joy_msg.axes]
         buttons = joy_msg.buttons
         
-        a0 = axes[0]
-        a1 = axes[1]
-        a2 = axes[2]
-        a3 = axes[3]
-        a4 = axes[4]
-        a5 = axes[5]
+        gauche_droite = axes[7]
+        avancer_reculer = axes[8] 
+        monter = axes[3]
+        descendre = axes[6]
+        A = buttons[1]
+        B = buttons[2]
+        X = buttons[3]
+        Y = buttons[4]
         # Publish arm movement commands
         rospy.loginfo("\n")
-        rospy.loginfo("avancer/reculer: %s", a0)
+        rospy.loginfo("gauche/droite: %s", gauche_droite)
+        rospy.loginfo("avancer/reculer: %s", avancer_reculer)
+        rospy.loginfo("monter: %s", monter)
+        rospy.loginfo("descendre: %s", descendre)
+        rospy.loginfo("A: %s", A)
+        rospy.loginfo("B: %s", B)
+        rospy.loginfo("X: %s", X)
+        rospy.loginfo("Y: %s", Y)
         rospy.loginfo("\n")
         #self.arm.set_joint_value_target(joint_positions)
         #self.arm.go()
