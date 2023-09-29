012052d73c7dc9c71dd2010e17185abf8be3c16b
Paul Daran <paul.daran@mines-ales.org>
2023-09-29 16:36:58 +0200

    modif fonctionnelles

--- ROS/Python/test_TeleopNode.py
+++ ROS/Python/test_TeleopNode.py
@@ -8,8 +8,8 @@ class TeleopNode:
     def __init__(self):
         rospy.init_node('teleop_node')
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
-        self.arm = MoveGroupCommander('arm_group')
-        self.arm.set_max_velocity_scaling_factor(0.1)
+        #self.arm = MoveGroupCommander('arm_group')
+        #self.arm.set_max_velocity_scaling_factor(0.1)
 
     def joy_callback(self, joy_msg):
         # Map joystick inputs to arm movements
@@ -18,20 +18,19 @@ class TeleopNode:
         axes = [round(value,3) for value in joy_msg.axes]
         buttons = joy_msg.buttons
         
-        gauche_droite = axes[7]
-        avancer_reculer = axes[8] 
-        monter = axes[3]
-        descendre = axes[6]
-        A = buttons[1]
-        B = buttons[2]
-        X = buttons[3]
-        Y = buttons[4]
-        # Publish arm movement commands
+        gauche_droite = axes[6]
+        avancer_reculer = axes[7] 
+        monter_descendre = axes[4]
+        A = buttons[0]
+        B = buttons[1]
+        X = buttons[2]
+        Y = buttons[3]
+        #Publish arm movement commands
         rospy.loginfo("\n")
+        rospy.loginfo(joy_msg.axes)
         rospy.loginfo("gauche/droite: %s", gauche_droite)
         rospy.loginfo("avancer/reculer: %s", avancer_reculer)
-        rospy.loginfo("monter: %s", monter)
-        rospy.loginfo("descendre: %s", descendre)
+        rospy.loginfo("monter/descendre: %s", monter_descendre)
         rospy.loginfo("A: %s", A)
         rospy.loginfo("B: %s", B)
         rospy.loginfo("X: %s", X)
