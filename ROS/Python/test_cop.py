ca3cb4abbed95fec7b6866b2112d265e9e20d2c9
Paul Daran <paul.daran@mines-ales.org>
2023-10-02 12:18:40 +0200

    thread

--- ROS/Python/test_cop.py
+++ ROS/Python/test_cop.py
@@ -1,8 +1,8 @@
-#Envoi des données de position du TCP à MoveIt + matrice d'état du TCP et incrémentation
 import rospy
 from moveit_commander import MoveGroupCommander 
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Joy
+
 
 class TeleopNode:
 
@@ -26,7 +26,6 @@ class TeleopNode:
         self.matrix_tcp = [[self.pose_TCP.position.x, self.pose_TCP.position.y, self.pose_TCP.position.z],
                            [self.pose_TCP.orientation.x, self.pose_TCP.orientation.y, self.pose_TCP.orientation.z, self.pose_TCP.orientation.w]]   
 
-        global data
         self.data = [0, 0, 0]
 
         #Initialisation du publisher
@@ -52,62 +51,53 @@ class TeleopNode:
         #Configuration de la pose cible dans MoveIt
         send_pose = self.g.set_pose_target(pose)
 
-        #Configuration de la fréquence de publication
-        rate = rospy.Rate(10) # 10hz
-
         #Envoi des instructions de déplacement en publiant la pose cible sur le topic approprié
-        self.pub.publish(send_pose)
-
-        #Remise à zéro des incréments?
-        # self.data.axes[0] = 0
-        # self.data.axes[1] = 0
-        # self.data.axes[3] = 0
+        if send_pose is not None:
+            self.pub.publish(send_pose)
 
     #Cette fonction tourne en boucle? 
     def joy_callback(self, joy_msg):
-        #Test print??
-        rospy.loginfo("bouclage joy_calback")
-
-        #Récupération des données du joystick
-        global axes, buttons
-        
-        axes = [round(value,3) for value in joy_msg.axes]
-        buttons = joy_msg.buttons
-        
-        # Utilisation des données joystick pour modifier la matrice de position du TCP
-        # axes[8] = x | joystick gauche av/ar?
-        if axes[8] > 0:
-            self.data[0] = 0.1
-        elif axes[8] < 0:
-            self.data[0] = -0.1
-        else:
-            self.data[0] = 0
-        
-        # axes[7] = y | joystick gauche d/g?
-        if axes[7] > 0:
-            self.data[1] = -0.1
-        elif axes[7] < 0:
-            self.data[1] = 0.1
-        else:
-            self.data[1] = 0
-        
-        # axes[3] = +z | Gachette droite? A voir quelles sont les datas envoyées depuis la gachette?
-        if axes[3] > 0:
-            self.data[2] = 0.1
-        elif axes[6] > 0:
-            self.data[2] = -0.1
-        else:
-            self.data[2] = 0
+        if joy_msg is not None:
+            #Récupération des données du joystick
+            axes = [round(value,3) for value in joy_msg.axes]
+            buttons = joy_msg.buttons
+            print(axes)
+            print(buttons)
+            
+            # Utilisation des données joystick pour modifier la matrice de position du TCP
+            # axes[8] = x | joystick gauche av/ar?
+            if axes[5] > 0:
+                self.data[0] = 0.1
+            elif axes[5] < 0:
+                self.data[0] = -0.1
+            else:
+                self.data[0] = 0
+            
+            # axes[7] = y | joystick gauche d/g?
+            if axes[4] > 0:
+                self.data[1] = -0.1
+            elif axes[4] < 0:
+                self.data[1] = 0.1
+            else:
+                self.data[1] = 0
+            
+            # axes[3] = +z | Gachette droite? A voir quelles sont les datas envoyées depuis la gachette?
+            if axes[1] > 0:
+                self.data[2] = 0.1
+            elif axes[1] > 0:
+                self.data[2] = -0.1
+            else:
+                self.data[2] = 0
+                    
+            #Plan/execute | Bouton A
+            if buttons[1] == 1:
+                self.g.execute(self.g.plan())
                 
-        #Plan/execute | Bouton A
-        if buttons[1] == 1:
-            self.g.execute(self.g.plan())
 
 if __name__ == '__main__':
     node = TeleopNode()
 
+    rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         node.set_pose()
-    
-
-
+        rate.sleep()
