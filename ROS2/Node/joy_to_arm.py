cd0ffa49d1c7e60ee817c433def4ba09ab67cbad
Paul Daran <paul.daran@mines-ales.org>
2023-10-30 21:37:18 +0100

    node

---
+++ ROS2/Node/joy_to_arm.py
@@ -0,0 +1,2 @@
+# Node sous ROS2 pour venir lire sur le topic joy_node les entrées d'une manette, réaliser un traitement grace à une matrice d'état qui décrit la position du TCP, matrice d'état qui est publié et interprété par Moveit2
+
--- ROS2/Package_bras/README.txt
+++ ROS2/Package_bras/README.txt
@@ -1,1 +1,2 @@
 Package brut après extraction de Fusion360 et modifiaction par Moveit2 Setup Assistant.
+-> arm_hand marche bien une fois passé dans setupassistant
