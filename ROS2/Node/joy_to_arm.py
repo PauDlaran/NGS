3bd535d2b96dc959f65f604f0462839db15e08fb
Paul Daran <paul.daran@mines-ales.org>
2023-11-12 19:15:50 +0100

    node

--- ROS2/Node/joy_to_arm.py
+++ ROS2/Node/joy_to_arm.py
@@ -1,2 +1,1 @@
 # Node sous ROS2 pour venir lire sur le topic joy_node les entrées d'une manette, réaliser un traitement grace à une matrice d'état qui décrit la position du TCP, matrice d'état qui est publié et interprété par Moveit2
-
